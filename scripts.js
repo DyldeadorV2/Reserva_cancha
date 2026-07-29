15d7506c371d0af690c8164166063457b420699c
DyldeadorV2 <dylanfcitoflow@gmail.com>
2026-07-29 16:33:32 -0300

    login terminado

--- scripts.js
+++ scripts.js
@@ -5,14 +5,57 @@ function reserva() {
     alert(`Reserva confirmada para ${cancha} el ${fecha} a las ${hora}`);
 }
 function login() {
+    try {
     const email = document.getElementById("email-input").value;
     const password = document.getElementById("password-input").value;
+
     if (email === "" || password === "") {
         alert("Por favor, complete todos los campos.");
         return;
     }
-    const url = `http://localhost/reserva_cancha/index.php?caso=login&email=${email}&password=${password}`;
- 
-    
-    
+    const url = `http://localhost/reserva_cancha/backend/index.php?caso=login&email=${email}&contraseña=${password}`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                alert("Inicio de sesión exitoso");
+                window.location.href = "index.html";
+            }
+        })
+
+    } catch (error) {
+        console.error("Error al iniciar sesión:", error);
+        alert("Ocurrió un error al intentar iniciar sesión.");
+    }
 }
+
+
+function register() {
+    try {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const teléfono = document.getElementById("phone").value;
+    const nombre = document.getElementById("name").value;
+
+    if (email === "" || password === "" || teléfono === "" || nombre === "") {
+        alert("Por favor, complete todos los campos.");
+        return;
+    }
+    const url = `http://localhost/reserva_cancha/backend/index.php?caso=register&email=${email}&contraseña=${password}&teléfono=${telefono}&nombre=${nombre}`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                alert("Registro exitoso");
+                window.location.href = "login.html";
+            }
+        })
+
+    } catch (error) {
+        console.error("Error al registrar:", error);
+        alert("Ocurrió un error al intentar registrarse.");
+    }
+}
+
